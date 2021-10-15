6643baa279c6ce7624ee04638b724a3225dd20cd
장민 <>
2021-10-15 19:08:27 +0900

    ㅠㅠㅠㅠㅠ

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
+from datetime import datetime
+
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
 client = MongoClient("mongodb://localhost:27017/")
-db = client.dbStock2
-
-from datetime import datetime
+db = client.dbStock
 
 
 @app.route('/')
@@ -14,27 +14,75 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/post', methods=['POST'])
+@app.route('/article', methods=['POST'])
 def save_post():
-    post_url_receive = request.form['post_url_give']
-    post_comment_receive = request.form['post_comment_give']
-    views_receive = request.form['views_give']
-    time = datetime.today().strftime("%Y.%m.%d %H:%M:%S")
-    doc = {'제목': post_url_receive, '내용': post_comment_receive, '날짜' : time, '조회수' : views_receive}
-    db.users.insert_one(doc)
-    return {"result": "포스팅 성공!"}
+    title = request.form.get('title')
+    content = request.form.get('content')
+    article_count = db.article.count()
+    if article_count == 0:
+        max_value = 1
+    else:
+        max_value = db.article.find_one(sort=[("idx", -1)])['idx'] + 1
+
+    post = {
+        'idx': max_value,
+        'title': title,
+        'content': content,
+        'read_count': 0,
+        'reg_date': datetime.now()
+    }
+    db.article.insert_one(post)
+    return {"result": "success"}
 
 
-@app.route('/post', methods=['GET'])
-def get_post():
-    post_list = list(db.users.find({}, {'_id': False}).sort('날짜', -1))
-    return {"result": post_list}
+@app.route('/articles', methods=['GET'])
+def get_posts():
+    order = request.args.get('order')
+    if order == "desc":
+        articles = list(db.article.find({}, {'_id': False}).sort([("read_count", -1)]))
+    else:
+        articles = list(db.article.find({}, {'_id': False}).sort([("reg_date", -1)]))
 
-@app.route('/post', methods=['DELETE'])
+    for a in articles:
+        a['reg_date'] = a['reg_date'].strftime('%Y.%m.%d %H:%M:%S')
+
+    return jsonify({"articles": articles})
+
+
+@app.route('/article', methods=['DELETE'])
 def delete_post():
-    titie_receive = request.form['delete_give']
-    db.users.delete_one({'제목': titie_receive})
-    return {"result": "삭제하였습니다!"}
+    idx = request.args.get('idx')
+    db.article.delete_one({'idx': int(idx)})
+    return {"result": "success"}
+
+
+@app.route('/article', methods=['GET'])
+def get_post():
+    idx = request.args['idx']
+    article = db.article.find_one({'idx': int(idx)}, {'_id': False})
+    return jsonify({"article": article})
+
+
+@app.route('/article', methods=['PUT'])
+def update_post():
+    idx = request.form.get('idx')
+    title = request.form.get('title')
+    content = request.form.get('content')
+    db.article.update_one({'idx': int(idx)}, {'$set': {'title': title, 'content': content}})
+    return {"result": "success"}
+
+
+@app.route('/article/<idx>', methods=['PUT'])
+def update_read_count(idx):
+    db.article.update_one({'idx': int(idx)}, {'$inc': {'read_count': 1}})
+    article = db.article.find_one({'idx': int(idx)}, {'_id': False})
+    return jsonify({"article": article})
+
+# @app.route('/list/', methods=['GET'])
+# def _list():
+#     page = request.args.get('page', type=int, default=1)
+#     question_list = question_list.paginate(page, per_page=2)
+#     return render_template('question/question_list.html', question_list=question_list)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
