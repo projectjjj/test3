bd14dc40ec61c69c1fafe4e86d3d052e7d9e189b
장민 <>
2021-10-29 19:24:51 +0900

    ㅠㅠㅠㅠㅠ

--- app.py
+++ app.py
@@ -1,21 +1,84 @@
-from datetime import datetime
-
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request, redirect, url_for
 from pymongo import MongoClient
+import jwt
+import hashlib
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client.dbStock
 
+SECRET_KEY = 'SPARTA'
 
 @app.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    token_receive = request.cookies.get('mylovelytoken')
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        status = (token_receive == True)
+        user_info = db.user.find_one({"id" : payload["id"]})
+        print(user_info)
+        return render_template('index.html', user_info=user_info,status=status)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("index", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("index", msg="로그인 정보가 존재하지 않습니다."))
+
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+
+    pw_hash = hashlib.sha256(pw_receive.encode('utf-8')).hexdigest()
+    result = db.user.find_one({'id': id_receive, 'pw': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': id_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "id": username_receive,                               # 아이디
+        "pw": password_hash,                                  # 비밀번호
+    }
+    db.user.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.user.find_one({"id": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
 
 
 @app.route('/article', methods=['POST'])
 def save_post():
+    token_receive = request.cookies.get('mylovelytoken')
+    payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+    user = db.user.find_one({"id": payload["id"]})
     title = request.form.get('title')
     content = request.form.get('content')
     article_count = db.article.count()
@@ -29,7 +92,8 @@ def save_post():
         'title': title,
         'content': content,
         'read_count': 0,
-        'reg_date': datetime.now()
+        'reg_date': datetime.now(),
+        'id' : user["id"]
     }
     db.article.insert_one(post)
     return {"result": "success"}
@@ -38,15 +102,40 @@ def save_post():
 @app.route('/articles', methods=['GET'])
 def get_posts():
     order = request.args.get('order')
-    if order == "desc":
-        articles = list(db.article.find({}, {'_id': False}).sort([("read_count", -1)]))
+    per_page = request.args.get('perPage')
+    cur_page = request.args.get('curPage')
+    search_title = request.args.get('searchTitle')
+    search_condition = {}
+    if search_title is not None:
+        search_condition = {"title": {"$regex": search_title}}
+        #서치 한 것 에서만 카운트를 해야하기때문에.
+
+    limit = int(per_page)
+    skip = limit * (int(cur_page) - 1)
+    #2페이지에 가면 앞에 거는 스킵하고 그 다음 데이터부터 보여져야 하니까,,, 모든 데이터에서 쪼개서 스킵스킵
+    #안가져올 데이터는 스킵해버리는 것.
+    total_count = db.article.find(search_condition).count()
+    total_page = int(total_count / limit) + (1 if total_count % limit > 0 else 0) #나누고 나머지가 있으면 이라는 뜻. 삼항식.
+
+    if order == "desc": #조회수정렬
+        articles = list(db.article.find(search_condition, {'_id': False})
+                        .sort([("read_count", -1)]).skip(skip).limit(limit))
     else:
-        articles = list(db.article.find({}, {'_id': False}).sort([("reg_date", -1)]))
+        articles = list(db.article.find(search_condition, {'_id': False})
+                        .sort([("reg_date", -1)]).skip(skip).limit(limit))
 
     for a in articles:
         a['reg_date'] = a['reg_date'].strftime('%Y.%m.%d %H:%M:%S')
 
-    return jsonify({"articles": articles})
+    paging_info = { #정렬한 것을 프론트로 넘겨주기 위해서 json 으로 정리.
+        "totalCount": total_count,
+        "totalPage": total_page,
+        "perPage": per_page,
+        "curPage": cur_page,
+        "searchTitle": search_title
+    }
+
+    return jsonify({"articles": articles, "pagingInfo": paging_info})
 
 
 @app.route('/article', methods=['DELETE'])
@@ -78,11 +167,6 @@ def update_read_count(idx):
     article = db.article.find_one({'idx': int(idx)}, {'_id': False})
     return jsonify({"article": article})
 
-# @app.route('/list/', methods=['GET'])
-# def _list():
-#     page = request.args.get('page', type=int, default=1)
-#     question_list = question_list.paginate(page, per_page=2)
-#     return render_template('question/question_list.html', question_list=question_list)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
